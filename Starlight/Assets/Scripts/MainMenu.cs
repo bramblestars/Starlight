7f0340eaedc0d6fefe074a870f2db654e561d8bc
Sanjay Salem <38601117+sanjaysalem17@users.noreply.github.com>
2019-02-18 21:59:36 -0500

    fixed commit
    main menu ui

---
+++ Starlight/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
