242764f4809d4e9e7e1cb3a09bec16e1f4edceef
bramblestars <txxcui@gmail.com>
2019-04-05 17:38:03 -0400

    hi

---
+++ Starlight/Assets/Scripts/Stars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour
+{
+
+    private StarText starCounter;
+
+    void Awake()
+    {
+        starCount = GameObject.Find("CoinText").getComponent<StarText>
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            print("You picked up a star!");
+
+        //Update GUI
+        starCounter.starCount++;
+
+        gameObject.SetActive(false);
+        
+    }
+}
